543a91f73b3804ad86594d1a10b5d2325d47cd80
danilenko2109 <bohdan.danilenko@gmail.com>
2026-04-06 16:07:23 +0200

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const requestRoutes = require("../routes/requestRoutes");
 
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: ["https://escort-xrus.vercel.app"], credentials: true }));
 app.use(express.json());
 
 app.get("/api/health", (_req, res) => {
